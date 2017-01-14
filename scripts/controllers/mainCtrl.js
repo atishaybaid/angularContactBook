b17fa91f299ecc994407aec4da5e45d4052462cf
atishaybaid <atishaybaid@gmail.com>
2017-01-14 20:32:35 +0530

    inlineEdit done

--- scripts/controllers/mainCtrl.js
+++ scripts/controllers/mainCtrl.js
@@ -1,6 +1,22 @@
-angular.module('contactBook').controller('mainCtrl', ['$scope', function($scope){
+angular.module('contactBook').controller('mainCtrl', ['$scope','data' ,function($scope,data){
 
 $scope.name = 'atishay';
+console.log(data);
+
+$scope.peopleList = data.peopleList;
+
+
+$scope.editCell = function(item,field) {
+	console.log(item);
+	item[field] = true;
+}
+
+
+$scope.checkDoneEditing = function(event,item,field){
+	if(event.keyCode === 13){
+		item[field] = false;
+	}
+}
 
 	
 }])
