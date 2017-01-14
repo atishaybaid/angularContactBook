b67a6deb2b372b6842381bbc616378cded336c42
atishaybaid <atishaybaid@gmail.com>
2017-01-15 02:56:24 +0530

    add contact implemented

--- scripts/controllers/mainCtrl.js
+++ scripts/controllers/mainCtrl.js
@@ -1,31 +1,65 @@
-angular.module('contactBook').controller('mainCtrl', ['$scope','data' ,function($scope,data){
+angular.module('contactBook').controller('mainCtrl', ['$scope','data','$mdDialog',function($scope,data,$mdDialog){
 
 $scope.name = 'atishay';
-console.log(data);
 
-/*$scope.peopleList = data.peopleList;*/
+
+
 var promise =  data.getPeopleList();
+
 promise.then(function(res){
 	$scope.peopleList = res;
-})
+});
 
 $scope.editCell = function(item,field) {
-	console.log(item);
 	item[field] = true;
-}
+};
 
 
 $scope.checkDoneEditing = function(event,item,field){
 	if(event.keyCode === 13){
 		item[field] = false;
 	}
-}
+};
 
 
 $scope.stopEditting = function(item,field){
-	console.log("stopEditing called");
 	item[field] = false;
-}
+};
 
+
+ $scope.showAdvanced = function(ev) {
+    $mdDialog.show({
+      controller: DialogController,
+      templateUrl: '../../addContact.html',
+      parent: angular.element(document.body),
+      targetEvent: ev,
+      clickOutsideToClose:true,
+      fullscreen: false, // Only for -xs, -sm breakpoints.
+      scope:$scope
+    })
+    .then(function(answer) {
+      
+    }, function() {
+ 
+    });
+  };
+
+
+	$scope.clickLisned = function(){
+  		console.log("click clickLisned");
+  	}
+
+  function DialogController($scope, $mdDialog) {
+  	$scope.newContact ={name:'',phone:'',address:'',fav:false};
+
+  	$scope.saveContact = function(){
+  		$scope.peopleList.push($scope.newContact);
+  		$mdDialog.cancel();
+  	}
+
+  	$scope.closeDialog = function(){
+  		$mdDialog.cancel();
+  	};
+  }
 	
 }])
